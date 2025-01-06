09d8646ecc4af711e31d2c9e2bd71dd2f766a5cf
Thiago <tsmonteiro@gmail.com>
2025-01-06 12:09:36 -0300

    setFileContent

--- webapp_utils/lib/project_functions.dart
+++ webapp_utils/lib/project_functions.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:webapp_utils/folder_node.dart';
 import 'package:sci_tercen_client/sci_client.dart';
 import 'package:sci_tercen_client/sci_client_service_factory.dart' as tercen;
@@ -16,7 +18,6 @@ class ProjectFunctions {
   ProjectFunctions._internal();
 
   Future<void> loadFolderStructure(String projectId) async {
-
     if (projectId != "") {
       List<dynamic> results = await Future.wait([
         _fetchRemoteFolderDocuments("", projectId, recursive: true),
@@ -34,11 +35,12 @@ class ProjectFunctions {
     }
   }
 
-
-  Future<FolderDocument> getOrCreateFolder(String projectId, String owner, String name, {String? parentId}) async {
+  Future<FolderDocument> getOrCreateFolder(
+      String projectId, String owner, String name,
+      {String? parentId}) async {
     var candidateFolders = _getDocuments(name, true, parentId: parentId);
 
-    if( candidateFolders.isNotEmpty ){
+    if (candidateFolders.isNotEmpty) {
       return candidateFolders.first as FolderDocument;
     }
 
@@ -57,14 +59,60 @@ class ProjectFunctions {
     return folder;
   }
 
+  Future<FileDocument> getOrCreateFile(
+      String projectId, String owner, String name,
+      {String? parentId, String contentType = "application/json"}) async {
+    var factory = tercen.ServiceFactory();
+    var candidateFolders = _getDocuments(name, false, parentId: parentId);
 
+    if (candidateFolders.isNotEmpty) {
+      //Folder exists. If more than one combination of name + parentId exists, return the first found
+      return await factory.fileService.get(candidateFolders.first.id);
+    }
 
-  List<Document> getProjectFiles(){
-    if( !structureLoaded){
+    var doc = FileDocument();
+
+    doc.name = name;
+    doc.isHidden = false;
+    doc.folderId = parentId ?? "";
+    doc.projectId = projectId;
+    doc.acl = Acl()..owner = owner;
+    doc.metadata.contentType = "application/json";
+    doc.dataUri = "";
+    doc = _setFileContent(doc, {});
+    doc = await factory.fileService.create(doc);
+
+    await loadFolderStructure(projectId);
+
+    return doc;
+  }
+
+  dynamic getFileContent(FileDocument fileDoc) {
+    if (fileDoc.metadata.contentType == "application/json") {
+      return json.decode(fileDoc.getMeta("file.content")!);
+    } else {
+      return fileDoc.getMeta("file.content")!;
+    }
+  }
+
+  Future<void> updateFileContent(FileDocument fileDoc, Map content) async {
+    var factory = tercen.ServiceFactory();
+    fileDoc = _setFileContent(fileDoc, content);
+    // ignore: invalid_return_type_for_catch_error
+    await factory.fileService.update(fileDoc).catchError((e) => Logger()
+        .log(level: Logger.INFO, message: "Unable to update model state file"));
+    ;
+  }
+
+  List<Document> getProjectFiles() {
+    if (!structureLoaded) {
       Logger().log(message: "Project file structure has not been loaded");
     }
 
-    return folderTreeRoot.getDescendants(folders: true, documents: true).map((e) => e.document).toList();
+    return folderTreeRoot
+        .getDescendants(folders: true, documents: true)
+        .map((e) => e.document)
+        .toList();
   }
 
   Future<List<ProjectDocument>> _fetchRemoteFolderDocuments(
@@ -134,17 +182,33 @@ class ProjectFunctions {
     return docs;
   }
 
-  List<Document> _getDocuments(String name, bool isFolder, {String? parentId}){
-    var candidateFolderNodes = folderTreeRoot.getDescendants(folders: true, documents: true).where((e) => e.document.name == name);
+  List<Document> _getDocuments(String name, bool isFolder, {String? parentId}) {
+    var candidateFolderNodes = folderTreeRoot
+        .getDescendants(folders: true, documents: true)
+        .where((e) => e.document.name == name);
 
-    if( parentId != null && candidateFolderNodes.isNotEmpty ){
-      if( isFolder ){
-        candidateFolderNodes = candidateFolderNodes.where((e) => (e.document as FolderDocument).folderId == parentId);
-      }else{
-        candidateFolderNodes = candidateFolderNodes.where((e) => (e.document as ProjectDocument).folderId == parentId);
+    if (parentId != null && candidateFolderNodes.isNotEmpty) {
+      if (isFolder) {
+        candidateFolderNodes = candidateFolderNodes
+            .where((e) => (e.document as FolderDocument).folderId == parentId);
+      } else {
+        candidateFolderNodes = candidateFolderNodes
+            .where((e) => (e.document as ProjectDocument).folderId == parentId);
       }
-    }    
+    }
 
     return candidateFolderNodes.map((e) => e.document).toList();
+  }
+
+  FileDocument _setFileContent(FileDocument fileDoc, dynamic content) {
+    if (content is Map) {
+      fileDoc.addMeta("file.content", json.encode(content));
+    } else if (content is String) {
+      fileDoc.addMeta("file.content", content);
+    } else {
+      throw Exception("Unsupported file content type");
+    }
+
+    return fileDoc;
   }
 }
