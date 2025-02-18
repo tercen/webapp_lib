642bef15688ef24cccf30a4637a38cbadc007fc7
Thiago <tsmonteiro@gmail.com>
2025-02-18 10:16:00 -0300

    int type

--- webapp_components/lib/components/upload_table_component.dart
+++ webapp_components/lib/components/upload_table_component.dart
@@ -81,7 +81,7 @@ class UploadTableComponent extends UploadFileComponent {
     print("Parsing ${values.first} - ${double.tryParse(values.first)}");
     
     if( checkAll(values, isNumeric) ){
-      dataType = "numeric"; //checkAll(values, isInt) ? "int" : "double";
+      dataType = checkAll(values, isInt) ? "int32" : "double";
     }
 
     print("Adding column $colName of type $dataType ($values)");
