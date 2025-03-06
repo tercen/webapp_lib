1a8073092bcff1caf12baf8e48d808f6c8f9e8a4
Thiago <tsmonteiro@gmail.com>
2025-03-06 14:36:41 -0300

    default init

--- webapp_ui_commons/lib/styles/styles.dart
+++ webapp_ui_commons/lib/styles/styles.dart
@@ -1,16 +1,23 @@
 
+import 'package:webapp_ui_commons/styles/default_style.dart';
 import 'package:webapp_ui_commons/styles/style_base.dart';
 
 class Styles {
   final Map<String, dynamic> styleMap = {};
+  bool isInit = false;
 
   static final Styles _instance = Styles._internal();
   
   factory Styles() {
+    
     return _instance;
   }
   
-  Styles._internal();
+  Styles._internal(){
+    if(!isInit ){
+      init([DefaultStyle()]);
+    }
+  }
 
   void init( List<StyleBase> styles ) async {
     for( var style in styles ){
@@ -22,7 +29,7 @@ class Styles {
 
   dynamic operator [](String key){
     if( !styleMap.keys.contains(key)){
-      throw Exception("Inavlid Style key $key");
+      throw Exception("Invalid Style key $key");
     }
     return styleMap[key];
   }
