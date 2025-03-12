417a04ad5864108d646178dce8c27c50f907f83f
Thiago <tsmonteiro@gmail.com>
2025-03-12 11:27:55 -0300

    workflwo setting fields

--- webapp_utils/lib/model/workflow_setting.dart
+++ webapp_utils/lib/model/workflow_setting.dart
@@ -2,9 +2,11 @@ class WorkflowSetting {
   final String name;
   final String value;
   final String type;
+  final String description;
+  final bool isSingleSelection;
   final List<String> options = [];
 
-  WorkflowSetting( this.name, this.value, this.type, { List<String> opOptions = const []} ){
+  WorkflowSetting( this.name, this.value, this.type, this.description, { List<String> opOptions = const [], this.isSingleSelection = false} ){
     if( opOptions.isNotEmpty ){
       options.addAll(opOptions);
     }
