15089ced9523bcca0074f1ed7f00c7f08f6d0f3a
Thiago <tsmonteiro@gmail.com>
2025-03-12 11:29:03 -0300

    step info

--- webapp_utils/lib/model/workflow_setting.dart
+++ webapp_utils/lib/model/workflow_setting.dart
@@ -1,4 +1,6 @@
 class WorkflowSetting {
+  final String stepName;
+  final String stepId;
   final String name;
   final String value;
   final String type;
@@ -6,7 +8,7 @@ class WorkflowSetting {
   final bool isSingleSelection;
   final List<String> options = [];
 
-  WorkflowSetting( this.name, this.value, this.type, this.description, { List<String> opOptions = const [], this.isSingleSelection = false} ){
+  WorkflowSetting( this.stepName, this.stepId, this.name, this.value, this.type, this.description, { List<String> opOptions = const [], this.isSingleSelection = false} ){
     if( opOptions.isNotEmpty ){
       options.addAll(opOptions);
     }
