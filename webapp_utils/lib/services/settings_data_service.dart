14fc8597e8569450f33e95c7ef3c5c05fda989b5
Thiago <tsmonteiro@gmail.com>
2025-03-14 13:57:44 -0300

    getStepId

--- webapp_utils/lib/services/settings_data_service.dart
+++ webapp_utils/lib/services/settings_data_service.dart
@@ -1,39 +1,45 @@
 import 'package:flutter/services.dart';
 import 'package:json_string/json_string.dart';
+import 'package:sci_tercen_client/sci_client.dart';
 import 'package:webapp_model/settings/required_template.dart';
 import 'package:webapp_model/settings/settings_filter.dart';
 import 'package:webapp_model/settings/workflow_steps.dart';
 import 'package:webapp_model/settings/template_config.dart';
-import 'package:sci_tercen_client/sci_client_service_factory.dart' as tercen;
-import 'package:webapp_utils/functions/workflow_utils.dart';
 
-class SettingsDataService{
+
+class SettingsDataService {
   static final SettingsDataService _singleton = SettingsDataService._internal();
-  
+
   factory SettingsDataService() {
     return _singleton;
   }
 
   SettingsDataService._internal();
 
-  
   var settingsFilters = SettingsFilter(filters: []);
   var workflowStepsMapper = WorkflowSteps(steps: []);
   var templateConfig = TemplateConfig(repos: []);
 
-
-  bool hasFilter( String screenName ){
+  bool hasFilter(String screenName) {
     return settingsFilters.filters.any((filter) => filter.screen == screenName);
   }
 
-
   List<RequiredTemplate> get requiredWorkflows => templateConfig.repos;
 
+  String getStepId(String workflowRef, String stepRef) {
+    try {
+      return workflowStepsMapper.steps
+          .firstWhere((step) =>
+              step.shortName == stepRef && step.workflowRef == workflowRef)
+          .stepId;
+    } catch (e) {
+      throw ServiceError(500, "Required Step does not exist",
+          "Required step $stepRef does not exist in $workflowRef. Please check the Workflo Step Mapper configuration in the assets.");
+    }
+  }
 
-
-
-  Future<void> loadTemplateConfig(String assetPath ) async {
-    if( assetPath == ""){
+  Future<void> loadTemplateConfig(String assetPath) async {
+    if (assetPath == "") {
       return;
     }
     var assetString = await rootBundle.loadString(assetPath);
@@ -42,10 +48,8 @@ class SettingsDataService{
     templateConfig = TemplateConfig.fromJson(jsonString.decodedValueAsMap);
   }
 
-
-
-  Future<void> loadWorkflowStepMapper(String assetPath ) async {
-    if( assetPath == ""){
+  Future<void> loadWorkflowStepMapper(String assetPath) async {
+    if (assetPath == "") {
       return;
     }
     var assetString = await rootBundle.loadString(assetPath);
@@ -54,7 +58,7 @@ class SettingsDataService{
   }
 
   Future<void> loadSettingsFilter(String assetPath) async {
-    if( assetPath == ""){
+    if (assetPath == "") {
       return;
     }
     var assetString = await rootBundle.loadString(assetPath);
@@ -62,5 +66,4 @@ class SettingsDataService{
 
     settingsFilters = SettingsFilter.fromJson(jsonString.decodedValueAsMap);
   }
-
 }
