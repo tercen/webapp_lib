57529175881cb04cf2b0e87f52eb60e6e03192f1
Thiago <tsmonteiro@gmail.com>
2025-03-15 15:23:26 -0300

    setting load order

--- webapp_utils/lib/services/workflow_data_service.dart
+++ webapp_utils/lib/services/workflow_data_service.dart
@@ -10,7 +10,6 @@ import 'package:webapp_model/id_element_table.dart';
 import 'package:webapp_model/settings/required_template.dart';
 import 'package:webapp_model/settings/template_config.dart';
 
-
 import 'package:webapp_utils/functions/logger.dart';
 
 import 'package:webapp_utils/functions/workflow_utils.dart';
@@ -20,16 +19,15 @@ import 'package:webapp_utils/model/workflow_info.dart';
 import 'package:sci_tercen_client/sci_client.dart' as sci;
 import 'package:webapp_utils/model/workflow_setting.dart';
 
-
 class WorkflowDataService with DataCache {
   static final WorkflowDataService _singleton = WorkflowDataService._internal();
-  
+
   factory WorkflowDataService() {
     return _singleton;
   }
-  
+
   WorkflowDataService._internal();
-  
+
   // final List<WorkflowInfo> _requiredWorkflows = [];
   final Map<String, Workflow> _installedWorkflows = {};
   List<WorkflowSetting> workflowSettings = [];
@@ -61,14 +59,14 @@ class WorkflowDataService with DataCache {
     // await readWorkflowsFromLib();
   }
 
-
-  void addWorkflow( String iid, Workflow workflow ){
+  void addWorkflow(String iid, Workflow workflow) {
     _installedWorkflows[iid] = workflow;
   }
 
   List<Workflow> getTemplates() {
     return _installedWorkflows.values.toList();
   }
+
   Workflow getWorkflow(String key) {
     if (!_installedWorkflows.containsKey(key)) {
       throw ServiceError(500, "Failed to find workflow with key '$key'");
@@ -76,20 +74,18 @@ class WorkflowDataService with DataCache {
     return _installedWorkflows[key]!;
   }
 
-
-  Future<List<sci.Document>> readWorkflowsDocumentsFromLib({String? team, String? user}) async {
-
-    List<String> teamList = user == null || user.isEmpty? [] : [user];
-    if(team != null && team.isNotEmpty){
-      teamList.add( team );
+  Future<List<sci.Document>> readWorkflowsDocumentsFromLib(
+      {String? team, String? user}) async {
+    List<String> teamList = user == null || user.isEmpty ? [] : [user];
+    if (team != null && team.isNotEmpty) {
+      teamList.add(team);
     }
-    
 
     var factory = tercen.ServiceFactory();
 
     var libObjs = await factory.documentService
         .getLibrary('', [], ["Workflow"], [], 0, -1);
-    
+
     // var workflows = await factory.workflowService.list(libObjs.map((o) => o.id).toList());
     return libObjs;
   }
@@ -107,7 +103,7 @@ class WorkflowDataService with DataCache {
 
   //   var libObjs = await factory.documentService
   //       .getLibrary('', [], ["Workflow"], [], 0, -1);
-    
+
   //   var reqWkfs = _getRequiredWorkflowsIds(libObjs);
 
   //   var workflows = await factory.workflowService.list(reqWkfs[0]);
@@ -124,7 +120,7 @@ class WorkflowDataService with DataCache {
 
   // String _isInRepoFile(Document libObj) {
   //   for (var info in _requiredWorkflows) {
-      
+
   //     var isCorrectVersion = info.version == "NONE" || info.version == libObj.version;
   //     // print("${info.url == libObj.url.uri} :::: $isCorrectVersion");
   //     if (info.url == libObj.url.uri && isCorrectVersion) {
@@ -197,7 +193,17 @@ class WorkflowDataService with DataCache {
         var step = dataSteps[i];
         var op = operators[i];
         workflowSettings.addAll(op.properties.map((prop) {
-          print("PROPERTY: ${prop.name} -- ${prop.kind} -- ${prop.runtimeType}");
+          if (prop is EnumeratedProperty) {
+            var kind = prop.isSingleSelection ? "ListSingle" : "ListMultiple";
+            return WorkflowSetting(step.name, step.id, prop.name,
+                prop.defaultValue, kind, prop.description,
+                isSingleSelection: prop.isSingleSelection,
+                opOptions: prop.values);
+          }
+          if (prop is BooleanProperty) {
+            return WorkflowSetting(step.name, step.id, prop.name,
+                prop.defaultValue.toString(), "boolean", prop.description);
+          }
           if (prop is DoubleProperty) {
             return WorkflowSetting(step.name, step.id, prop.name,
                 prop.defaultValue.toString(), "double", prop.description);
@@ -205,17 +211,6 @@ class WorkflowDataService with DataCache {
           if (prop is StringProperty) {
             return WorkflowSetting(step.name, step.id, prop.name,
                 prop.defaultValue, "string", prop.description);
-          }
-          if (prop is BooleanProperty) {
-            return WorkflowSetting(step.name, step.id, prop.name,
-                prop.defaultValue.toString(), "boolean", prop.description);
-          }
-          if (prop is EnumeratedProperty) {
-            var kind = prop.isSingleSelection ? "ListSingle" : "ListMultiple";
-            return WorkflowSetting(step.name, step.id, prop.name,
-                prop.defaultValue, kind, prop.description,
-                isSingleSelection: prop.isSingleSelection,
-                opOptions: prop.values);
           }
 
           return WorkflowSetting(
@@ -253,7 +248,7 @@ class WorkflowDataService with DataCache {
         RenameRelation rr = relation as RenameRelation;
         l.addAll(_getSimpleRelations(rr.relation));
         break;
-      
+
       default:
     }
 
@@ -286,7 +281,8 @@ class WorkflowDataService with DataCache {
     List<Relation> rels = [];
     Map<String, List<String>> stepRelationMap = {};
     for (var stp in wkf.steps) {
-      var shouldIncludeStep = includeStepId.isEmpty || includeStepId.contains(stp.id);
+      var shouldIncludeStep =
+          includeStepId.isEmpty || includeStepId.contains(stp.id);
       if (stp.kind == "DataStep" && shouldIncludeStep) {
         DataStep dStp = stp as DataStep;
         var relList = _getSimpleRelations(dStp.computedRelation);
@@ -297,7 +293,7 @@ class WorkflowDataService with DataCache {
 
     var schList =
         await factory.tableSchemaService.list(rels.map((e) => e.id).toList());
-    
+
     for (var sch in schList) {
       var step = _getRelationStep(wkf, stepRelationMap, sch.id);
       if (_isFileSchema(sch)) {
@@ -314,49 +310,50 @@ class WorkflowDataService with DataCache {
         List<String> uniqueAddedNames = [];
         Table contentTable = Table();
         var isDev = Uri.base.hasPort && Uri.base.port > 10000;
-        
 
-        if( isDev ){
+        if (isDev) {
           // Avoid CORS issue with downloading image through browser request
-          contentTable = await factory.tableSchemaService.select(sch.id, [sch.columns[nameIdx].name,  ".content"], 0, sch.nRows);
+          contentTable = await factory.tableSchemaService.select(
+              sch.id, [sch.columns[nameIdx].name, ".content"], 0, sch.nRows);
 
           List<Pair> uniqueNameType = [];
-          for (var i = 0; i < tbl.nRows; i++){
+          for (var i = 0; i < tbl.nRows; i++) {
             var name = tbl.columns[0].values[i];
             var cType = tbl.columns[1].values[i];
 
-            if(!uniqueNameType.any((e) => e.key == name)){
+            if (!uniqueNameType.any((e) => e.key == name)) {
               uniqueNameType.add(Pair.from(name, cType));
             }
           }
 
-          
-
-          for( var nameContent in uniqueNameType ){
-            var isCorrectType = (contentTypes.any((contentType) =>
-                nameContent.value.contains(contentType))) ;
+          for (var nameContent in uniqueNameType) {
+            var isCorrectType = (contentTypes
+                .any((contentType) => nameContent.value.contains(contentType)));
 
             var filterInclude = nameFilter.isEmpty ||
-                  nameFilter.any((name) => nameContent.key.contains(name));
+                nameFilter.any((name) => nameContent.key.contains(name));
 
-            if (!excludedFiles.contains(nameContent.key) && filterInclude && isCorrectType) {
+            if (!excludedFiles.contains(nameContent.key) &&
+                filterInclude &&
+                isCorrectType) {
               uniqueAddedNames.add(nameContent.key);
               workflowNames.add(IdElement("", wkf.name));
               stepNames.add(step);
               filenames.add(IdElement("", nameContent.key));
 
               var bStr = "";
-              for (var i = 0; i < tbl.nRows; i++){
+              for (var i = 0; i < tbl.nRows; i++) {
                 var tname = tbl.columns[0].values[i];
-                if( nameContent.key == tname){
-                  var newBStr = String.fromCharCodes( base64Decode( contentTable.columns[1].values[i]) );
+                if (nameContent.key == tname) {
+                  var newBStr = String.fromCharCodes(
+                      base64Decode(contentTable.columns[1].values[i]));
                   bStr = "$bStr$newBStr";
                 }
               }
               bytes.add(IdElement("", bStr));
             }
           }
-        }else{
+        } else {
           for (var i = 0; i < tbl.nRows; i++) {
             if (contentTypes.any((contentType) =>
                 tbl.columns[1].values[i].contains(contentType))) {
@@ -373,25 +370,17 @@ class WorkflowDataService with DataCache {
 
                   contentTypeList.add(IdElement("", ct));
 
-                    
-
-
                   var bytesStream = factory.tableSchemaService
                       .getFileMimetypeStream(sch.id, tbl.columns[0].values[i]);
                   var imgBytes = await bytesStream.toList();
 
-                  
-
-                  bytes.add(IdElement(
-                      "", String.fromCharCodes(Uint8List.fromList(imgBytes[0]))));
-                
+                  bytes.add(IdElement("",
+                      String.fromCharCodes(Uint8List.fromList(imgBytes[0]))));
                 }
               }
             }
           }
-       
         }
-
       }
     }
 
@@ -405,7 +394,6 @@ class WorkflowDataService with DataCache {
 
     return tbl;
   }
-
 
   IdElement _getRelationStep(
       Workflow wkf, Map<String, List<String>> stepRelationMap, String schId) {
@@ -427,22 +415,25 @@ class WorkflowDataService with DataCache {
     return IdElement(stp.id, stp.name);
   }
 
-  
-  Future<IdElementTable> fetchImageData(IdElementTable workflowImageTable ) async {
+  Future<IdElementTable> fetchImageData(
+      IdElementTable workflowImageTable) async {
     assert(workflowImageTable.colNames.contains("workflow"));
     assert(workflowImageTable.colNames.contains("image"));
 
-    var uniqueWorkflowIds = workflowImageTable["workflow"].map((e) => e.id).toSet().toList();
-    var uniqueStepIds = workflowImageTable["image"].map((e) => e.id).toSet().toList();
+    var uniqueWorkflowIds =
+        workflowImageTable["workflow"].map((e) => e.id).toSet().toList();
+    var uniqueStepIds =
+        workflowImageTable["image"].map((e) => e.id).toSet().toList();
 
     var outTbl = IdElementTable();
     var factory = tercen.ServiceFactory();
-    var workflows = await  factory.workflowService.list(uniqueWorkflowIds);
-    for( var w in workflows ){
-      var newTbl = await fetchWorkflowImages(w, includeStepId: uniqueStepIds, force: true);
-      if( outTbl.colNames.isEmpty ){
+    var workflows = await factory.workflowService.list(uniqueWorkflowIds);
+    for (var w in workflows) {
+      var newTbl = await fetchWorkflowImages(w,
+          includeStepId: uniqueStepIds, force: true);
+      if (outTbl.colNames.isEmpty) {
         outTbl = newTbl;
-      }else{
+      } else {
         outTbl.append(newTbl);
       }
     }
@@ -450,104 +441,111 @@ class WorkflowDataService with DataCache {
     return outTbl;
   }
 
-  bool canCancelWorkflow(IdElementTable row){
-    return row["status"][0].label != "Done" && row["status"][0].label != "Failed" && row["status"][0].label != "Unknown";
+  bool canCancelWorkflow(IdElementTable row) {
+    return row["status"][0].label != "Done" &&
+        row["status"][0].label != "Failed" &&
+        row["status"][0].label != "Unknown";
   }
 
   Future<void> cancelWorkflow(IdElementTable row) async {
-
-    
     var workflowId = row["name"][0].id;
-
 
     var factory = tercen.ServiceFactory();
     var workflow = await factory.workflowService.get(workflowId);
 
     var taskId = workflow.meta.firstWhere((e) => e.key == "run.task.id").value;
     await factory.taskService.cancelTask(taskId);
-    
+
     await factory.workflowService.delete(workflow.id, workflow.rev);
-    
-    
   }
 
-  Future<List<Workflow>> fetchWorkflowsRemote(String projectId) async{
+  Future<List<Workflow>> fetchWorkflowsRemote(String projectId) async {
     var factory = tercen.ServiceFactory();
-    var projObjs = await factory.projectDocumentService.findProjectObjectsByLastModifiedDate(startKey: [projectId, '0000'], endKey: [projectId, '9999']);
-    var workflowIds = projObjs.where((e) => e.subKind == "Workflow").map((e) => e.id).toList();
+    var projObjs = await factory.projectDocumentService
+        .findProjectObjectsByLastModifiedDate(
+            startKey: [projectId, '0000'], endKey: [projectId, '9999']);
+    var workflowIds = projObjs
+        .where((e) => e.subKind == "Workflow")
+        .map((e) => e.id)
+        .toList();
 
     return await factory.workflowService.list(workflowIds);
   }
 
-
   Future<Map<String, String>> getWorkflowStatus(Workflow workflow) async {
     var meta = workflow.meta;
-    var results = {"status":"", "error":"", "finished":"true"};
+    var results = {"status": "", "error": "", "finished": "true"};
     results["status"] = "Unknown";
-    
 
-    if(meta.any((e) => e.key == "run.task.id")){
-
+    if (meta.any((e) => e.key == "run.task.id")) {
       var factory = tercen.ServiceFactory();
 
       List<String> currentOnQueuWorkflow = [];
       List<String> currentOnQueuStep = [];
       List<State> currentOnQueuStatus = [];
-      var compTasks = await factory.taskService.getTasks(["RunComputationTask"]);
-        for( var ct in compTasks ){
-          if( ct is RunComputationTask){
-            for( var p in ct.environment ){
-              if( p.key == "workflow.id"){
-                currentOnQueuWorkflow.add(p.value);
-              }
-              if( p.key == "step.id"){
-                currentOnQueuStep.add(p.value);
-                currentOnQueuStatus.add(ct.state);
-              }
+      var compTasks =
+          await factory.taskService.getTasks(["RunComputationTask"]);
+      for (var ct in compTasks) {
+        if (ct is RunComputationTask) {
+          for (var p in ct.environment) {
+            if (p.key == "workflow.id") {
+              currentOnQueuWorkflow.add(p.value);
+            }
+            if (p.key == "step.id") {
+              currentOnQueuStep.add(p.value);
+              currentOnQueuStatus.add(ct.state);
             }
           }
         }
-      
+      }
+
       var isRunning = currentOnQueuWorkflow.contains(workflow.id);
-      var isFail = workflow.steps.any((e) => e.state.taskState is FailedState );
+      var isFail = workflow.steps.any((e) => e.state.taskState is FailedState);
 
-
-      if( isFail ){
+      if (isFail) {
         results["status"] = "Failed";
-        results["error"] = meta.firstWhere((e) => e.key.contains("run.error"), orElse: () => Pair.from("", "")).value;
-        if( meta.any((e) => e.key == "run.error.reason")){
-          results["error"] = meta.firstWhere((e) => e.key == "run.error.reason").value;
-        }else{
-          results["error"] = "${results["error"]}\n\nNo Error Details were Provided.";
+        results["error"] = meta
+            .firstWhere((e) => e.key.contains("run.error"),
+                orElse: () => Pair.from("", ""))
+            .value;
+        if (meta.any((e) => e.key == "run.error.reason")) {
+          results["error"] =
+              meta.firstWhere((e) => e.key == "run.error.reason").value;
+        } else {
+          results["error"] =
+              "${results["error"]}\n\nNo Error Details were Provided.";
         }
         results["finished"] = isRunning ? "false" : "true";
-      }else{
+      } else {
         var status = isRunning ? "Running" : "Pending";
         var allInit = true;
         var allDone = true;
         results["finished"] = isRunning ? "false" : "true";
         bool isAllPending = true;
-        for( var s in workflow.steps ){
-          for( var i = 0; i < currentOnQueuStep.length; i++ ){
-            if( currentOnQueuStep[i] == s.id && currentOnQueuWorkflow[i] == workflow.id ){
-              if( currentOnQueuStatus[i] is! PendingState ){
+        for (var s in workflow.steps) {
+          for (var i = 0; i < currentOnQueuStep.length; i++) {
+            if (currentOnQueuStep[i] == s.id &&
+                currentOnQueuWorkflow[i] == workflow.id) {
+              if (currentOnQueuStatus[i] is! PendingState) {
                 isAllPending = false;
               }
-              status = currentOnQueuStatus[i] is PendingState ? "Pending" : "Running";
+              status = currentOnQueuStatus[i] is PendingState
+                  ? "Pending"
+                  : "Running";
             }
           }
 
-          if( status == "Pending" && !isAllPending ){
+          if (status == "Pending" && !isAllPending) {
             status = "Running";
           }
-         
+
           allInit = allInit && (s.state.taskState is InitState);
           allDone = allDone && (s.state.taskState is DoneState);
         }
-        if( allInit  ){
+        if (allInit) {
           status = "Not Started";
         }
-        if( allDone ){
+        if (allDone) {
           status = "Done";
         }
         results["status"] = status;
@@ -556,14 +554,13 @@ class WorkflowDataService with DataCache {
     return results;
   }
 
-
-
   Future<Workflow> fetchWorkflow(String id) async {
     var factory = tercen.ServiceFactory();
     return factory.workflowService.get(id);
   }
 
-  Future<List<Workflow>> fetchProjectWorkflows(List<Document> projectFiles) async {
+  Future<List<Workflow>> fetchProjectWorkflows(
+      List<Document> projectFiles) async {
     var workflowIds = projectFiles
         .where((e) => e.subKind == "Workflow")
         .map((e) => e.id)
@@ -575,24 +572,21 @@ class WorkflowDataService with DataCache {
         : await factory.workflowService.list(workflowIds);
   }
 
-  Future<void> updateFile( Document document,
-       String text) async {
-      var factory = tercen.ServiceFactory();
-      var downloadStream = factory.fileService.download(document.id);
-      var fileBytes = await downloadStream.toList();
+  Future<void> updateFile(Document document, String text) async {
+    var factory = tercen.ServiceFactory();
+    var downloadStream = factory.fileService.download(document.id);
+    var fileBytes = await downloadStream.toList();
 
-      var readmeTxt = utf8.decode(fileBytes[0]);
+    var readmeTxt = utf8.decode(fileBytes[0]);
 
-      var notes = text.split("\n").map((e) => "> $e").join("  \n");
-      notes += "  \n\n";
-      notes = "## Run Notes  \n$notes";
-      readmeTxt = notes + readmeTxt;
+    var notes = text.split("\n").map((e) => "> $e").join("  \n");
+    notes += "  \n\n";
+    notes = "## Run Notes  \n$notes";
+    readmeTxt = notes + readmeTxt;
 
-      var doc = await factory.fileService.get(document.id);
-      Stream<List> dataStream =
-          Stream.fromIterable(Iterable.castFrom([utf8.encode(readmeTxt)]));
-      factory.fileService.upload(doc, dataStream);
+    var doc = await factory.fileService.get(document.id);
+    Stream<List> dataStream =
+        Stream.fromIterable(Iterable.castFrom([utf8.encode(readmeTxt)]));
+    factory.fileService.upload(doc, dataStream);
   }
-
 }
-
