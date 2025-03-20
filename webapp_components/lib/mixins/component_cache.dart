70f97f2266a50d25e273928c64f62a85df4dd375
Thiago <tsmonteiro@gmail.com>
2025-03-20 14:26:05 -0300

    cache options

--- webapp_components/lib/mixins/component_cache.dart
+++ webapp_components/lib/mixins/component_cache.dart
@@ -1,16 +1,21 @@
 mixin ComponentCache {
   //Cache Control
   final Map<String, dynamic> _cacheMap = {};
+  bool useCache = true;
 
   dynamic getCachedValue(String key){
     return _cacheMap[key];
   }
 
   bool hasCachedValue(String key){
-    return _cacheMap.containsKey(key);
+    return useCache && _cacheMap.containsKey(key);
   }
 
   void addToCache(String key, dynamic value){
     _cacheMap[key] = value;
+  }
+
+  void clearCache(){
+    _cacheMap.clear();
   }
 }
