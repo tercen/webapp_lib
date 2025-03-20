27cca9542f5faa6e43a72a9b8df659286746bc27
Thiago <tsmonteiro@gmail.com>
2025-03-20 10:09:37 -0300

    action fields

--- webapp_components/lib/definitions/list_action.dart
+++ webapp_components/lib/definitions/list_action.dart
@@ -7,7 +7,9 @@ typedef RowActionCallback = Future<void> Function( List<String> row ) ;
 class ListAction{
   Icon actionIcon;
   CheckActionCallback? enabledCallback;
-  RowActionCallback? callback;
+  RowActionCallback callback;
+  String? description;
+  String? buttonLabel;
 
-  ListAction(this.actionIcon, this.callback, {this.enabledCallback});
+  ListAction(this.actionIcon, this.callback, {this.enabledCallback, this.description, this.buttonLabel});
 }
