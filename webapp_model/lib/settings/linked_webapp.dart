9703dc7b99597731296ba45cd84db907aa7e7ed8
Thiago <tsmonteiro@gmail.com>
2025-04-09 10:17:17 -0300

    Linked webapp operator

---
+++ webapp_model/lib/settings/linked_webapp.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class LinkedWebapp {
+
+  final String shortName;
+  final String url;
+  final String? version;
+
+  LinkedWebapp({required this.shortName, required this.url, required this.version});
+
+
+  factory LinkedWebapp.fromJson(Map<String, dynamic> json) =>
+      _$LinkedWebappFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LinkedWebappToJson(this);
+
+}
