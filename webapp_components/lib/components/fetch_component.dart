1ca218017b21db1b8940f6b43352611a3e0a4f44
Thiago <tsmonteiro@gmail.com>
2025-04-15 15:16:23 -0300

    dbg

--- webapp_components/lib/components/fetch_component.dart
+++ webapp_components/lib/components/fetch_component.dart
@@ -46,12 +46,14 @@ class FetchComponent with
 
   Future<bool> loadTable() async {
     if (!isInit) {
+      print("Load table init");
       isInit = true;
       busy();
       var cacheKey = getKey();
       if (useCache && cacheObj.hasCachedValue(cacheKey)) {
         dataTable = cacheObj.getCachedValue(cacheKey);
       } else {
+        print("\tCalling data fetch");
         startFuture("dataLoad", dataFetchCallback());
 
         dataTable = await waitResult("dataLoad"); //  await dataFetchCallback();
