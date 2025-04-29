8da472b5c46871be982be40448818f06156505f2
Thiago <tsmonteiro@gmail.com>
2025-04-29 14:31:52 -0300

    Fix for joinTables

--- webapp_utils/lib/functions/tercen_utils.dart
+++ webapp_utils/lib/functions/tercen_utils.dart
@@ -8,37 +8,37 @@ class TercenUtils {
     for( var i = 0; i < numCols; i++){
       newColumnValues.add([]);
     }
+    var tbl1Cols = tbl1.columns.where((col) => onl.any((onk) => col.name.contains(onk)) );
+    var tbl2Cols = tbl2.columns.where((col) => onr.any((onk) => col.name.contains(onk)) );
+    var tbl1AllCols = tbl1.columns.where((col) => !excludeColumns.contains(col.name) );
+    var tbl2AllCols = tbl2.columns.where((col) => !excludeColumns.contains(col.name) );
+
+    final tbl2Index = <String, List<int>>{};
+    for( var ri2 = 0; ri2 < tbl2.nRows; ri2++){
+      var key = tbl2Cols.map((col) => col.values[ri2]).join("_");
+      tbl2Index.putIfAbsent(key, () => []).add(ri2);
+    }
 
     for( var ri = 0; ri < tbl1.nRows; ri++){
-      var lKeyVals = tbl1.columns.where((col) => onl.contains(col.name)).map((col) => col.values[ri]).join("_");
-      
-      // ===================================
-      //GET Values in tbl2 that match lKeyVals
-      for( var ri2 = 0; ri2 < tbl2.nRows; ri2++){
-        var rKeyVals = tbl2.columns.where((col) => onr.contains(col.name)).map((col) => col.values[ri2]).join("_");
+      var lKeyVals = tbl1Cols.map((col) => col.values[ri]).join("_");
+      final tbl2RowIndices = tbl2Index[lKeyVals];
 
-        if( rKeyVals == lKeyVals ){
-
-          var ci = 0;
-
-          for( var col in tbl1.columns){
-            if( !excludeColumns.contains(col.name)){
-              newColumnValues[ci].add(col.values[ri]);
-              ci++;
+      if( tbl2RowIndices!= null ){
+        for( var ri2 in tbl2RowIndices ){
+           var combinedValues = <dynamic>[];
+            for (var col in tbl1AllCols  ) {
+              combinedValues.add(col.values[ri]);
             }
-          }
-
-          for( var col in tbl2.columns){
-            if( !excludeColumns.contains(col.name)){
-              newColumnValues[ci].add(col.values[ri2]);
-              ci++;
+            for (var col in tbl2AllCols) {
+              combinedValues.add(col.values[ri2]);
             }
-          }
-          break;
+
+            for (var i = 0; i < combinedValues.length; i++) {
+              newColumnValues[i].add(combinedValues[i]);
+            }
         }
       }
     }
-
 
     var newTable = Table();
     newTable.nRows = tbl1.nRows;
@@ -70,4 +70,5 @@ class TercenUtils {
 
     return newTable;
   }
+
 }
