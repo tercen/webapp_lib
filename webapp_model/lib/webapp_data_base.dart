bebfc294cab9b501e198d7f667d5e00a1271fece
Thiago <tsmonteiro@gmail.com>
2025-05-12 08:55:50 -0300

    dbg

--- webapp_model/lib/webapp_data_base.dart
+++ webapp_model/lib/webapp_data_base.dart
@@ -103,7 +103,10 @@ class WebAppDataBase with ChangeNotifier {
       if (contentString != "" && contentString != "{}") {
         _model =
             ViewState.fromJson(JsonString(contentString).decodedValueAsMap);
-        print("Loaded model:\n\t${_model.toJson()}");
+        print("Loaded model:\n");
+        for( var vo in _model.objects ){
+          print("\t${vo.key}: ${vo.values}");
+        }
       }
 
       if (loadNavigation == true) {
