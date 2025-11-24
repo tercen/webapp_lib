fcfe433f787677f7b3c17cdbd5a06be41facb278
Thiago <tsmonteiro@gmail.com>
2025-11-24 11:00:16 -0300

    Add `setOperator` method

--- webapp_core/lib/runner/utils/workflow/workflow_settings_utils.dart
+++ webapp_core/lib/runner/utils/workflow/workflow_settings_utils.dart
@@ -1,4 +1,5 @@
 import 'package:sci_tercen_client/sci_client.dart' as sci;
+import 'package:sci_tercen_client/sci_client_service_factory.dart' as tercen;
 import 'package:webapp_core/runner/utils/functions/logger.dart';
 
 class WorkflowSettingsUtils {
@@ -31,6 +32,60 @@ class WorkflowSettingsUtils {
       step.model.operatorSettings.environment[ei].value = value;
     }
 
+    return workflow;
+  }
+
+  static Future<sci.Workflow> setOperator({required sci.Workflow workflow,
+    required String operatorUrl, String operatorVersion = "latest",
+  required String stepId}) async {
+    final step = workflow.steps.whereType<sci.DataStep>().where((step) => step.id == stepId).firstOrNull;
+    if( step == null ){
+      throw sci.ServiceError(404, "step.not.found", "Step $stepId not found in workflow ${workflow.name} (${workflow.id})");
+    }
+
+
+    final operatorList = (await tercen.ServiceFactory()
+        .documentService
+        .getLibrary('', [], ["Operator"], [], 0, -1))
+        .where((op) => op.url.uri == operatorUrl)
+        .toList();
+
+    sci.Document? selectedOperator;
+
+    if( operatorVersion.isEmpty || operatorVersion != "latest"){
+      throw sci.ServiceError(400, "invalid.operator.version", "operatorVersion must be: (a) a semVer version. (b) a commit id; or (c) latest");
+    }
+
+    if (operatorVersion != "latest") {
+      // Match specific version
+      selectedOperator = operatorList
+          .where((op) => op.version == operatorVersion)
+          .firstOrNull;
+
+      if (selectedOperator == null) {
+        throw sci.ServiceError(404, "operator.version.not.found",
+            "Operator $operatorUrl version $operatorVersion not found");
+      }
+    } else {
+      // Get latest operator in library
+      operatorList.sort((a, b) {
+        final dateA = DateTime.parse(a.lastModifiedDate.value);
+        final dateB = DateTime.parse(b.lastModifiedDate.value);
+        return dateB.compareTo(dateA); // Most recent first
+      });
+
+      selectedOperator = operatorList.first;
+    }
+
+    final opObject = await tercen.ServiceFactory().operatorService.get(selectedOperator.id);
+    final opRef = sci.OperatorRef()
+      ..operatorId = opObject.id
+      ..version = opObject.version
+      ..operatorKind = opObject.kind
+      ..operatorSpec = opObject.operatorSpec
+      ..url = opObject.url;
+
+    step.model.operatorSettings.operatorRef = opRef;
     return workflow;
   }
 
